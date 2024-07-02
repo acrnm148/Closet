937eaac82e7288f424452cb685a83f29b61f958c
강수나 <acrnm148@gmail.com>
2024-07-03 06:18:05 +0900

    feat: first commit

---
+++ closet_back/src/main/java/closet/ClosetBackApplication.java
@@ -0,0 +1,13 @@
+package closet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClosetBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClosetBackApplication.class, args);
+    }
+
+}
